1ec943aef0887b80e6ddf7b7b5fbb4482499971b
Maxim Dashkov <maxim_dashkov@epam.com>
2018-02-15 13:00:52 +0300

    EPMFARMFE-1039 Создает функцию createOriginalArr(arr), которая рекурсивно перебирает каждый элемент массива и возвращает новый массива со значениями = квадратный корень из соотв. значений исходного массива - update

--- task_2.js
+++ task_2.js
@@ -1,22 +1,20 @@
-//Функция из Task_1 генерирующая тестовыый массив
+//Функция из Task_1 генерирующая тестовый массив
 function createArr(length) {
-    var resArr = [];
+    var resArr = new Array();
+
     for (var i = 0; i < length; i++) {
-        resArr[i] = i*i;
+        resArr.push(i * i);
     }
+
     return resArr;
 }
 
 function createOriginalArr(arr) {
-    var resArr = [];
-    function sqr() {
-        if (arr.length) {
-            resArr.push(Math.sqrt(arr.shift()));
-            sqr();
-        }
-    }
-    sqr();
-    return resArr;
+
+    if (arr.length) {
+        return (createOriginalArr(arr.slice(0, arr.length - 1))).concat(Math.sqrt(arr[arr.length - 1]));
+    } else return [];
 }
+
 console.log(createArr(5));
 console.log(createOriginalArr(createArr(5)));
