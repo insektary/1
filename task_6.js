0e127baf234e8c2c5be9dfb60533561504d04c57
Maxim Dashkov <maxim_dashkov@epam.com>
2018-02-20 19:17:48 +0300

    EPMFARMFE-1052 Создает функцию, которая возвращает результат функции (task_3) и время вызова. Не чаще чем раз в пол секунды - update 2

--- task_6.js
+++ task_6.js
@@ -1,26 +1,33 @@
 var callbackInside = function() {
     var storage = {};
-    return function(arg) {
-        if (arg in storage) return storage[arg];
-        else {
-            storage[arg] = Math.round(Math.random() * (1000 - 5) + 5);
 
-            return storage[arg];
+    var func = function(arg) {
+        if (!storage[arg]) {
+            storage[arg] = Math.round(Math.random() * (1000 - 5) + 5);
         }
-    }
+
+        return storage[arg];
+    };
+
+    return func;
 };
+
 var callback = callbackInside();
 
 var timerInside = function() {
     var lastTime = 0;
+    var DELAY = 500; //ms
 
-    return function(arg) {
-        var DELAY = 500; //ms
+    var func = function(arg) {
         var interval = Date.now() - lastTime;
         lastTime = Date.now();
 
-        if (interval > DELAY) return callback(arg) + ' ' + Date();
-    }
+        if (interval > DELAY) {
+            return callback(arg) + ' ' + Date();
+        }
+    };
+
+    return func;
 };
 
 var timer = timerInside();
