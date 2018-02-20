6316d0f9647a079aadd35b53d9957c2fad8fea59
Maxim Dashkov <maxim_dashkov@epam.com>
2018-02-20 19:22:56 +0300

    EPMFARMFE-1051 Создает функцию, которая вызывается не чаще, чем один раз в 2 секунды и возвращает результат функции (Task_3) - update 2

--- task_5.js
+++ task_5.js
@@ -1,25 +1,29 @@
 var callbackInside = function() {
     var storage = {};
-    return function(arg) {
+
+    var func = function(arg) {
         if (arg in storage) return storage[arg];
         else {
             storage[arg] = Math.round(Math.random() * (1000 - 5) + 5);
 
             return storage[arg];
         }
-    }
+    };
+
+    return func;
 };
+
 var callback = callbackInside();
 
 var timerInside = function() {
     var lastTime = 0;
 
-    return function(arg) {
+    var func = function(arg) {
         var interval = Date.now() - lastTime;
         lastTime = Date.now();
 
         if (interval > 2000) return callback(arg);
-    }
-};
+    };
 
-var timer = timerInside();
+    return func;
+};
