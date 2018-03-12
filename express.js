155faf037c94ce543998069d567888ef5d1d9499
Maxim Dashkov <maxim_dashkov@epam.com>
2018-03-12 13:23:39 +0300

    EPMFARMFE-1056 - methods of express.js

--- express.js
+++ express.js
@@ -5,36 +5,46 @@ var express = function() {
     var server = 'off';
 
     return {
+        req: {},
+        res: {},
+        currentRequestMethod: '',
         routeTable: {
             get: {},
             use: {}
         },
+        index: null,
         listen: function(port) {
             server = 'on';
         },
-        caller: function(array, index) {
-            if (!array[index + 1]) return;
-            index++;
-            array[index]('', '', array[index + 1]);
-        },
         getResponse: function(url, method) {
             if (server === 'off') {
-                return;
+                console.log('server in not started');
             } else if (url in this.routeTable.use) {
-                this.caller(this.routeTable.use[url], -1);
+                this.currentRequestMethod = method;
+                this.index = 0;
 
-                this.routeTable[method.toLowerCase()][url]();
+                this.routeTable.use[url][0]();
             }
         },
         use: function(url, callback) {
+            var obj = this;
+
             if (!(url in this.routeTable.use)) {
                 this.routeTable.use[url] = [];
             }
 
-            this.routeTable.use[url].push(callback);
+            this.routeTable.use[url].push(function() {
+                obj.index++;
+
+                if (obj.routeTable.use[url][obj.index]) {
+                    callback(obj.req, obj.res, obj.routeTable.use[url][obj.index]);
+                } else {
+                    callback(obj.req, obj.res, obj.routeTable[obj.currentRequestMethod.toLowerCase()][url]);
+                }
+            });
         },
         get: function(url, callback) {
-            this.routeTable.get[url] = callback;
+            this.routeTable.get[url] = callback.bind(this, this.req, this.res);
         }
     }
 };
@@ -45,42 +55,19 @@ var express = function() {
 var app = express();
 
 app.use('/', function(req, res, next) {
-    console.log('prev1');
-    //req.input = EXPECTED_VALUE;
-    next()
+    req.input = EXPECTED_VALUE;
+    next();
 });
 
 app.use('/', function(req, res, next) {
-    console.log('prev2');
-    //console.log(req.input);
-    next()
+    console.log(req.input);
+    next();
 });
 
 app.get('/', function(req, res) {
-    console.log('get');
-    //console.log(req.input === EXPECTED_VALUE) // true
+    console.log(req.input === EXPECTED_VALUE) // true
 });
 
 app.listen(PORT);
 
 app.getResponse('/', 'GET');
-
-
-// var express = require('express');
-// var app = express();
-//
-// app.use('/', function(req, res, next) {
-//     console.log(next);
-//     next();
-// });
-//
-// app.get('/', function(req, res) {
-//     console.log('get come');
-//     res.send('11');
-// });
-//
-// app.listen(3000, function() {
-//     console.log('server is running');
-// });
-//
-// console.log('end');
