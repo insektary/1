8ce59e9f0ed2ddbd05be9abd3c612e8481ac2593
Maxim Dashkov <maxim_dashkov@epam.com>
2018-03-16 19:42:33 +0300

    EPMFARMFE-1065 Создает методы работы с Yandex translate API

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": ["eslint-config-es6",
+        "eslint-config-airbnb-es5"],
+    "rules": {
+        "linebreak-style": [
+            "off",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            0,
+            "never"
+        ],
+        "func-names": [
+            "error",
+            "never"
+        ],
+        "no-console": "off",
+        "no-require": [
+            "off"
+        ],
+        "consistent-return": [
+            "off"
+        ],
+        "no-loop-func": [
+            "off"
+        ],
+        "indent": [
+            "error",
+            4,
+            { "SwitchCase": 1 }
+        ],
+        "no-extend-native": [
+            "error",
+            { "exceptions": ["Array", "Function"] }]
+    }
+};
