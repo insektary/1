2a03dcf33c82b03d050b0cfd31c346baa814cb84
Maxim Dashkov <maxim_dashkov@epam.com>
2018-03-21 17:17:01 +0300

    Delete task_2.js
    EPMFARMFE-1066 - Adds a new es6 features, deletes unnecessary code, renames the constants, introduces the use of esm module

--- network.js
+++ network.js
@@ -1,120 +1,123 @@
 class Network {
-    constructor(name, networkAddress) {
-        this.networkAddress = networkAddress;
-        this.listOfClients = [];
-        this.name = name;
-        this.CONSTS = {
-            DELAY: 10000,
-            ONLINE: 'ONLINE',
-            TEMPORARILY_OFFLINE: 'TEMPORARILY_OFFLINE',
-            SERVER: 'SERVER',
-            ADDRESS_CAPACITY: 999,
-        };
-    }
+	constructor(name, networkAddress) {
+		this.networkAddress = networkAddress;
+		this.listOfClients = [];
+		this.name = name;
+		this.CONSTS = {
+			DELAY: 10000,
+			ONLINE: 'ONLINE',
+			TEMPORARILY_OFFLINE: 'TEMPORARILY_OFFLINE',
+			SERVER: 'SERVER',
+			ADDRESS_CAPACITY: 999,
+		};
+	}
 
-    registerInNetwork(name, type, feedbackMethod) {
-        const foundClient = this.listOfClients.find(
-            ({ clientName }) => clientName === name
-        );
+	registerInNetwork(name, type, feedbackMethod) {
+		const foundClient = this.listOfClients.find(client => client.name === name);
 
-        if (foundClient) {
-            foundClient.status = this.CONSTS.ONLINE;
-            clearTimeout(foundClient.timer);
+		if (foundClient) {
+			foundClient.status = this.CONSTS.ONLINE;
+			clearTimeout(foundClient.timer);
 
-            return foundClient.address;
-        }
+			return foundClient.address;
+		}
 
-        if (this.listOfClients.length > this.CONSTS.ADDRESS_CAPACITY) {
-            return;
-        }
+		if (this.listOfClients.length > this.CONSTS.ADDRESS_CAPACITY) {
+			return;
+		}
 
-        const address = this._findFreeAddress();
+		const address = this._findFreeAddress();
 
-        this.listOfClients.push({
-            name,
-            address,
-            type,
-            status: this.CONSTS.ONLINE,
-            feedbackMethod,
-        });
+		this.listOfClients.push({
+			name,
+			address,
+			type,
+			status: this.CONSTS.ONLINE,
+			feedbackMethod,
+		});
 
-        return address;
-    }
+		return address;
+	}
 
-    _findFreeAddress() {
-        let freeAddress = 1;
+	_findFreeAddress() {
+		let freeAddress = 1;
 
-        const checkFreeAddress = array => {
-            return array.find(({ address }) => address === freeAddress);
-        };
+		const checkFreeAddress = array => {
+			return array.find(client => client.address === freeAddress);
+		};
 
-        while (checkFreeAddress(this.listOfClients)) {
-            freeAddress++;
-        }
+		while (checkFreeAddress(this.listOfClients)) {
+			freeAddress++;
+		}
 
-        return freeAddress;
-    }
+		return freeAddress;
+	}
 
-    _finalRemoveClient(clientIP) {
-        this.listOfClients.splice(
-            this.listOfClients.findIndex(({ address }) => address === clientIP),
-            1
-        );
-    }
+	_finalRemoveClient(clientIP) {
+		this.listOfClients.splice(
+			this.listOfClients.findIndex(client => client.address === clientIP),
+			1
+		);
+	}
 
-    removeClient(clientIP) {
-        const foundClient = this.listOfClients.find(
-            ({ address }) => address === clientIP
-        );
+	removeClient(clientIP) {
+		const foundClient = this.listOfClients.find(
+			client => client.address === clientIP
+		);
 
-        foundClient.status = this.CONSTS.TEMPORARILY_OFFLINE;
-        foundClient.timer = setTimeout(
-            this._finalRemoveClient.bind(this),
-            this.CONSTS.DELAY,
-            clientIP
-        );
-    }
+		foundClient.status = this.CONSTS.TEMPORARILY_OFFLINE;
+		foundClient.timer = setTimeout(
+			this._finalRemoveClient.bind(this),
+			this.CONSTS.DELAY,
+			clientIP
+		);
+	}
 
-    changeAddress(currentAddress, wishAddress) {
-        const isBusyAddress = this.listOfClients.find(
-            ({ address }) => address === wishAddress
-        );
+	changeAddress(currentAddress, wishAddress) {
+		const isBusyAddress = this.listOfClients.find(
+			client => client.address === wishAddress
+		);
 
-        if (isBusyAddress) {
-            return;
-        }
+		if (isBusyAddress) {
+			return;
+		}
 
-        this.listOfClients.find(
-            ({ address }) => address === currentAddress
-        ).address = wishAddress;
+		this.listOfClients.find(
+			client => client.address === currentAddress
+		).address = wishAddress;
 
-        return wishAddress;
-    }
+		return wishAddress;
+	}
 
-    showAllClients() {
-        console.log('\n');
+	showAllClients() {
+		console.log('\n');
 
-        this.listOfClients.forEach(({ status, address, name, type }) => {
-            if (status === this.CONSTS.ONLINE) {
-                console.log(
-                    `${this.networkAddress}.${address} ${name} ${type}`
-                );
-            }
-        });
-    }
+		this.listOfClients.forEach(client => {
+			if (client && client.status === this.CONSTS.ONLINE) {
+				console.log(
+					`${this.networkAddress}.${client.address} ${client.name} ${
+						client.type
+					}`
+				);
+			}
+		});
+	}
 
-    findServers() {
-        return this.listOfClients.filter(
-            ({ type, status }) =>
-                type === this.CONSTS.SERVER && status === this.CONSTS.ONLINE
-        );
-    }
+	findServers() {
+		return this.listOfClients.filter(
+			client =>
+				client.type === this.CONSTS.SERVER &&
+				client.status === this.CONSTS.ONLINE
+		);
+	}
 
-    requestToServer(serverName, requestInfo) {
-        this.listOfClients
-            .find(({ name }) => name === serverName)
-            .feedbackMethod(requestInfo);
-    }
+	requestToServer(serverName, requestInfo) {
+		let requestedServer = this.listOfClients.find(
+			client => client.name === serverName
+		);
+
+		requestedServer.feedbackMethod(requestInfo);
+	}
 }
 
 export default Network;
