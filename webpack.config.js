0753737696aedbb4c9fdb3c8756e088aa818ca8c
Maxim Dashkov <maxim_dashkov@epam.com>
2018-03-30 20:00:02 +0300

    EPMFARMFE-1030 Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        //publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'css-hot-loader', MiniCssExtractPlugin.loader,  "css-loader", "less-loader"
+                ]
+            }
+        ]
+    },
+    mode: 'development',
+    devServer: {
+        stats: 'errors-only',
+        contentBase: __dirname,
+        hot: true,
+        port: 8080
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+            chunkFilename: "[id].css"
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Project demo',
+            template: 'src/html/index.html'
+        }),
+    ]
+};
