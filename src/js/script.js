f7c36adcca5d966edb0f684e8bdf3099c204242a
Maxim Dashkov <maxim_dashkov@epam.com>
2018-04-26 11:59:19 +0300

    EPMFARMFE-1070 regular expresses moved to object, numbers changed to constants, XMLXttpRequest changed to $.ajax, Array.prototype changed to []

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 require('./libs/jquery.js');
 require('./libs/jquery.maskedinput-1.2.2.js');
 require('./libs/jquery.autocomplete.js');
@@ -17,17 +18,24 @@ const weight = document.querySelector('.weight-value__value');
 const sizeItems = document.querySelectorAll('.size-item');
 const sizeBox = document.querySelector('.size-box');
 const countButton = document.querySelector('.count-button');
-const ZIP_EXP = /[0-9]{6}/;
-const RU_EXP = /^[а-яА-Я]+$/;
-const EMAIL_EXP = /^.+\@.{2,}\..{2,}$/i;
-const xhr = new XMLHttpRequest();
+const regExps = {
+    zip: /[0-9]{6}/,
+    ru: /^[а-яА-Я]+$/,
+    email: /^.+\@.{2,}\..{2,}$/i,
+    default: /^[а-яА-Я0-9]+$/
+};
+const RATIO_KG = 1;
+const RATIO_LBS = 2.2;
+const RANGE_DELAY = 500; //ms
+const CONVENTIONAL_UNIT = 5;
+
 let PRICES;
 let TIMER;
 
 const countWeight = () => {
     const weightValue = document.querySelector('.weight-range').value;
     const measure = document.querySelector('.weight-measure').value;
-    const ratio = (measure === 'kg') ? 1 : 2.2;
+    const ratio = (measure === 'kg') ? RATIO_KG : RATIO_LBS;
 
     weight.innerHTML = Math.round(weightValue * ratio * 100) / 100;
 
@@ -35,13 +43,13 @@ const countWeight = () => {
         clearTimeout(TIMER);
     }
 
-    TIMER = setTimeout(countAll, 500);
+    TIMER = setTimeout(countAll, RANGE_DELAY);
 };
 
 const countSize = ({ target }) => {
     const measureClass = target.className.split('__')[1];
 
-    Array.prototype.forEach.call(sizeItems, (item) => {
+    [].forEach.call(sizeItems, (item) => {
         if (item.className.split('--')[1] === measureClass) {
             item.innerHTML = target.value;
         }
@@ -51,7 +59,7 @@ const countSize = ({ target }) => {
         clearTimeout(TIMER);
     }
 
-    TIMER = setTimeout(countAll, 500);
+    TIMER = setTimeout(countAll, RANGE_DELAYS);
 };
 
 const showSizeBox = ({ target }) => {
@@ -82,20 +90,20 @@ const checkRegExp = (target) => {
 
     switch (target.name) {
         case 'zip':
-            REGEXP = ZIP_EXP;
+            REGEXP = RegExp.zip;
             break;
         case 'name':
         case 'subname':
         case 'fathername':
         case 'city':
         case 'street':
-            REGEXP = RU_EXP;
+            REGEXP = regExps.ru;
             break;
         case 'email':
-            REGEXP = EMAIL_EXP;
+            REGEXP = regExps.email;
             break;
         default:
-            REGEXP = /^[а-яА-Я0-9]+$/;
+            REGEXP = regExps.default;
     }
 
     return REGEXP;
@@ -181,23 +189,23 @@ const countAll = () => {
     const isSises = document.querySelector('.size__checkbox').checked;
     const country = document.querySelector('.address-country').value;
 
-    Array.prototype.forEach.call(priceItems, (item) => {
+    [].forEach.call(priceItems, (item) => {
         const companyName = item.className.split('--')[1];
         const price = PRICES.find((companyPrice) => companyPrice.name === companyName);
 
         if(isSises) {
-            item.innerHTML = Math.round(5 * price.ratio_country[country]
+            item.innerHTML = Math.round(CONVENTIONAL_UNIT * price.ratio_country[country]
                 * (weightValue * price.ratio_weight)
                 * (lengthValue * price.ratio_size)
                 * (widthValue * price.ratio_size)
                 * (heightValue * price.ratio_size));
         } else {
-            item.innerHTML = Math.round(5 * price.ratio_country[country]
+            item.innerHTML = Math.round(CONVENTIONAL_UNIT * price.ratio_country[country]
                 * (weightValue * price.ratio_weight));
         }
     });
 
-    Array.prototype.forEach.call(termsItem, (item) => {
+    [].forEach.call(termsItem, (item) => {
         const companyName = item.className.split('--')[1];
         const price = PRICES.find((companyPrice) => companyPrice.name === companyName);
 
@@ -213,23 +221,23 @@ document.querySelector('.size__checkbox').addEventListener('click', countAll);
 document.querySelector('.count-button').addEventListener('click', onSubmit);
 document.querySelector('.address-country').addEventListener('change', countAll);
 document.querySelector('.input-wrapper__input--phone').addEventListener('keyup', checkPhone);
-Array.prototype.forEach.call(document.querySelectorAll('.size-range'), (range) => {
+[].forEach.call(document.querySelectorAll('.size-range'), (range) => {
     range.addEventListener('input', countSize);
     range.addEventListener('change', countSize);
 });
-Array.prototype.forEach.call(document.querySelectorAll('.input-wrapper__input'), (input) => {
+[].forEach.call(document.querySelectorAll('.input-wrapper__input'), (input) => {
     input.setAttribute('correctly', 'false');
     input.addEventListener('input', checkInput);
     input.addEventListener('blur', removeHelp);
 });
 
-xhr.open('GET', 'prices.json', false);
-xhr.send();
-if (xhr.status !== 200) {
-    alert('Невозможно загрузить стоимость доставки');
-} else {
-    PRICES = JSON.parse(xhr.responseText);
-}
+$.ajax({
+    url: "prices.json",
+    dataType: "json",
+    success: (data) => {
+        PRICES = data;
+    }
+});
 
 $(($) => {
     $.mask.definitions['~']='[+-]';
