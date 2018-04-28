ec28d958167cb99e2ef37022e92de5836372384a
Maxim Dashkov <maxim_dashkov@epam.com>
2018-04-28 14:57:31 +0300

    EPMFARMFE-1070 adds new jQuery functions, separates logic and data, corrects syntax, adds babel-polyfill

--- src/js/script.js
+++ src/js/script.js
@@ -3,31 +3,22 @@
 require('./libs/jquery.js');
 require('./libs/jquery.maskedinput-1.2.2.js');
 require('./libs/jquery.autocomplete.js');
+require("babel-polyfill");
 
-if (!Array.prototype.find) {
-    Array.prototype.find = function(callback, context) {
-        for (let i = 0; i < this.length; i++) {
-            if (callback.call(context, this[i], i, this)) {
-                return this[i];
-            }
-        }
-    }
-}
-
-const priceItems = $('.company-price__item');
-const termsItem = $('.company-term__term');
-const weight = $('.weight-range');
-const length = $('.size-range__length');
-const width = $('.size-range__width');
-const height = $('.size-range__height');
-const isSizes = $('.size__checkbox');
-const country = $('.address-country');
-const weightDisplay = $('.weight-value__value');
-const sizeItems = $('.size-item');
-const sizeBox = $('.size-box');
-const countButton = $('.count-button');
+const $priceItems = $('.company-price__item');
+const $termsItem = $('.company-term__term');
+const $weight = $('.weight-range');
+const $length = $('.size-range__length');
+const $width = $('.size-range__width');
+const $height = $('.size-range__height');
+const $isSizes = $('.size__checkbox');
+const $country = $('.address-country');
+const $weightDisplay = $('.weight-value__value');
+const $sizeItems = $('.size-item');
+const $sizeBox = $('.size-box');
+const $countButton = $('.count-button');
+const $measure = $('.weight-measure');
 const form = $('.control').get(0);
-const measure = $('.weight-measure');
 const regExps = {
     zip: /[0-9]{6}/,
     email: /^.+\@.{2,}\..{2,}$/i,
@@ -41,46 +32,49 @@ const regExps = {
     flat: /^[а-яА-Я0-9]+$/
 };
 const RATIO_KG = 1;
+const IS_KG = 'kg';
 const RATIO_LBS = 2.2;
 const RANGE_DELAY = 500; //ms
 const CONVENTIONAL_UNIT = 5;
+const ROUND_RATIO = 100;
+const CHECKBOX = 'checkbox';
 
-let PRICES;
-let TIMER;
+let prices;
+let timer;
 
 const countWeight = () => {
-    const ratio = (measure.attr('value') === 'kg') ? RATIO_KG : RATIO_LBS;
+    const ratio = ($measure.val() === IS_KG) ? RATIO_KG : RATIO_LBS;
 
-    weightDisplay.text(Math.round(weight.attr('value') * ratio * 100) / 100);
+    $weightDisplay.text(Math.round($weight.val() * ratio * ROUND_RATIO) / ROUND_RATIO);
 
-    if (TIMER) {
-        clearTimeout(TIMER);
+    checkTimer();
+};
+
+const checkTimer = () => {
+    if (timer) {
+        clearTimeout(timer);
     }
 
-    TIMER = setTimeout(countAll, RANGE_DELAY);
+    timer = setTimeout(countAll, RANGE_DELAY);
 };
 
 const countSize = ({ target }) => {
     const measureClass = target.className.split('__')[1];
 
-    [].forEach.call(sizeItems, (item) => {
+    [].forEach.call($sizeItems, (item) => {
         if (item.className.split('--')[1] === measureClass) {
             item.innerHTML = target.value;
         }
     });
 
-    if (TIMER) {
-        clearTimeout(TIMER);
-    }
-
-    TIMER = setTimeout(countAll, RANGE_DELAY);
+    checkTimer();
 };
 
-const showSizeBox = ({ target }) => {
-    if (target.checked) {
-        sizeBox.css('display', 'flex');
+const showSizeBox = ({ target: { checked } }) => {
+    if (checked) {
+        $sizeBox.css('display', 'flex');
     } else {
-        sizeBox.css('display', 'none');
+        $sizeBox.css('display', 'none');
     }
 };
 
@@ -88,29 +82,27 @@ const onSubmit = () => {
     const data = {};
 
     [].forEach.call(form.elements, ({ name, type, checked, value }) => {
-        if (type === 'checkbox') {
+        if (type === CHECKBOX) {
             data[name] = checked;
         } else {
             data[name] = value;
         }
     });
-
-    console.log(data);
 };
 
 const checkInput = (event) => {
-    const target = $(event.target);
-    const checkIsCorrect = regExps[target.attr('name')].test(target.attr('value'));
-    const value = target.attr('value');
+    const $target = $(event.target);
+    const checkIsCorrect = regExps[$target.attr('name')].test($target.val());
+    const value = $target.val();
 
     if (value && checkIsCorrect) {
-        target.attr('correctly', 'true').removeClass('input-wrapper__input--warning').addClass('input-wrapper__input--checked');
+        $target.attr('correctly', 'true').removeClass('input-wrapper__input--warning').addClass('input-wrapper__input--checked');
 
         removeHelp(event);
     } else if (value && !checkIsCorrect) {
-        target.attr('correctly', 'false').addClass('input-wrapper__input--warning').siblings().css('display', 'block');
+        $target.attr('correctly', 'false').toggleClass('input-wrapper__input--warning', true).siblings().css('display', 'block');
     } else {
-        target.attr('correctly', 'false').addClass('input-wrapper__input--warning');
+        $target.attr('correctly', 'false').toggleClass('input-wrapper__input--warning', true);
 
         removeHelp(event);
     }
@@ -126,52 +118,70 @@ const checkAll = () => {
     if ([].some.call($('.input-wrapper__input'),
             (input) => input.getAttribute('correctly') === 'false')) {
 
-        countButton.removeClass('count-button--enable').attr('disabled', 'disable');
+        $countButton.removeClass('count-button--enable').attr('disabled', 'disable');
     } else {
-        countButton.addClass('count-button--enable').removeAttr('disabled');
+        $countButton.addClass('count-button--enable').removeAttr('disabled');
     }
 
 };
 
 const countAll = () => {
-    [].forEach.call(priceItems, (item) => {
+    $priceItems.each((index, item) => {
         const companyName = item.className.split('--')[1];
-        const price = PRICES.find((companyPrice) => companyPrice.name === companyName);
+        const price = prices.find(({ name }) => name === companyName);
 
-        if(isSizes.attr('checked')) {
-            item.innerHTML = Math.round(CONVENTIONAL_UNIT * price.ratio_country[country.attr('value')]
-                * (weight.attr('value') * price.ratio_weight)
-                * (length.attr('value') * price.ratio_size)
-                * (width.attr('value') * price.ratio_size)
-                * (height.attr('value') * price.ratio_size));
+        if($isSizes.attr('checked')) {
+            item.innerHTML = Math.round(CONVENTIONAL_UNIT * price.ratio_country[$country.val()]
+                * ($weight.val() * price.ratio_weight)
+                * ($length.val() * price.ratio_size)
+                * ($width.val() * price.ratio_size)
+                * ($height.val() * price.ratio_size));
         } else {
-            item.innerHTML = Math.round(CONVENTIONAL_UNIT * price.ratio_country[country.attr('value')]
-                * (weight.attr('value') * price.ratio_weight));
+            item.innerHTML = Math.round(CONVENTIONAL_UNIT * price.ratio_country[$country.val()]
+                * ($weight.val() * price.ratio_weight));
         }
     });
 
-    [].forEach.call(termsItem, (item) => {
+    [].forEach.call($termsItem, (item) => {
         const companyName = item.className.split('--')[1];
-        const price = PRICES.find((companyPrice) => companyPrice.name === companyName);
+        const price = prices.find((companyPrice) => companyPrice.name === companyName);
 
-        item.innerHTML = price.therms[country.attr('value')];
+        item.innerHTML = price.therms[$country.val()];
     })
 };
 
-weight.change(countWeight);
-measure.change(countWeight);
-isSizes.click(showSizeBox);
-isSizes.click(countAll);
-countButton.click(onSubmit);
-country.change(countAll);
+$weight.change(countWeight);
+$measure.change(countWeight);
+$isSizes.click(showSizeBox);
+$isSizes.click(countAll);
+$countButton.click(onSubmit);
+$country.change(countAll);
 $('.size-range').change(countSize);
 $('.input-wrapper__input').keyup(checkInput).blur(removeHelp).attr('correctly', 'false');
 
 $.ajax({
-    url: "prices.json",
-    dataType: "json",
+    url: 'prices.json',
+    dataType: 'json',
     success: (data) => {
-        PRICES = data;
+        prices = data;
+    }
+});
+
+$.ajax({
+    url: 'supported_cities.json',
+    dataType: 'json',
+    success: (supportedCities) => {
+        $(document).ready(() => {
+
+            $('#city').autocompleteArray(supportedCities,
+                {
+                    delay: 10,
+                    minChars: 1,
+                    matchSubset: 1,
+                    autoFill: true,
+                    maxItemsToShow: 10
+                });
+        });
     }
 });
 
@@ -179,26 +189,4 @@ $(($) => {
     $.mask.definitions['~']='[+-]';
 
     $('#phone').mask('(999) 999-99-99');
-});
-
-$(document).ready(() => {
-
-    $("#city").autocompleteArray([
-        'Абакан',
-        'Азамар',
-        'Актау',
-        'Актобе',
-        'Алдан',
-        'Томск',
-        'Трехгорный',
-        'Туапсе',
-        'Тула',
-        'Рязань'],
-    {
-        delay: 10,
-        minChars: 1,
-        matchSubset: 1,
-        autoFill: true,
-        maxItemsToShow: 10
-    });
 });
