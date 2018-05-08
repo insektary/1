de7fb5258d250d0f961c5542805db4ce7b508a24
Maxim Dashkov <maxim_dashkov@epam.com>
2018-05-08 12:10:35 +0300

    EPMFARMFE-1075 changes if - else to switch - case construction

--- src/script.js
+++ src/script.js
@@ -9,30 +9,35 @@ let browser;
 let version;
 let begin;
 
-if (user.indexOf('Edge') > 0) {
-    browser = 'Edge';
-    begin = user.indexOf('Edge') + 5;
-    version = user.substring(begin);
-} else if (user.indexOf('OPR') > 0) {
-    browser = 'Opera';
-    begin = user.indexOf('OPR') + 4;
-    version = user.substring(begin);
-} else if (user.indexOf('Firefox') > 0) {
-    browser = 'Firefox';
-    begin = user.indexOf('Firefox') + 8;
-    version = user.substring(begin);
-} else if (user.indexOf('.NET') > 0) {
-    browser = 'Internet Explorer';
-    begin = user.indexOf('rv') + 3;
-    version = user.substring(begin, checkVersionLength(user, begin));
-} else if (navigator.vendor === 'Google Inc.' && user.indexOf('OPR') < 0) {
-    browser = 'Chrome';
-    begin = user.indexOf('Chrome') + 7;
-    version = user.substring(begin, checkVersionLength(user, begin));
+switch (true) {
+    case user.includes('Edge'):
+        browser = 'Edge';
+        begin = user.indexOf('Edge') + 5;
+        version = user.substring(begin);
+        break;
+    case user.includes('OPR'):
+        browser = 'Opera';
+        begin = user.indexOf('OPR') + 4;
+        version = user.substring(begin);
+        break;
+    case user.includes('Firefox'):
+        browser = 'Firefox';
+        begin = user.indexOf('Firefox') + 8;
+        version = user.substring(begin);
+        break;
+    case user.includes('.NET'):
+        browser = 'Internet Explorer';
+        begin = user.indexOf('rv') + 3;
+        version = user.substring(begin, checkVersionLength(user, begin));
+        break;
+    case (navigator.vendor === 'Google Inc.' && !user.includes('OPR')):
+        browser = 'Chrome';
+        begin = user.indexOf('Chrome') + 7;
+        version = user.substring(begin, checkVersionLength(user, begin));
+        break;
 }
 
 console.log(browser);
 console.log(version);
 console.log(location.href);
 console.log(os);
-
