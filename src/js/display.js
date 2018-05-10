60213e8edcca4baabbfa3835388ab3b4523d005f
Maxim Dashkov <maxim_dashkov@epam.com>
2018-05-10 17:23:17 +0300

    creates form of online-translator with using Yandex-API

--- src/js/display.js
+++ src/js/display.js
@@ -1,14 +1,53 @@
-const title = document.querySelector('.title');
+const background = document.querySelector('.grey-background--hidden');
 const output = document.querySelector('.output-form');
+const body = document.querySelector('body');
+const NETWORK_ERROR = 'Сервис недоступен';
+const SERVICE_ERROR = 'Невозможно выполнить перевод';
+const MODAL_WINDOW = 'modal-window';
+const CLOSE_BUTTON = 'modal-window__close';
+const TEXTFIELD = 'modal-window__textfield';
+const CROSS_CODE = '&#10060';
+const BACKGROUND_VISIBLE = 'grey-background grey-background--visible';
+const BACKGROUND_HIDDEN = 'grey-background grey-background--hidden';
+
+const closeModeWindow = () => {
+    document.querySelector('.modal-window').remove();
+    background.className = BACKGROUND_HIDDEN;
+
+    clearOutput();
+};
 
 export const showErrorMessage = () => {
-    title.innerHTML = 'Сервис недоступен';
+    showModalWindow(NETWORK_ERROR);
 };
 
 export const showWarningMessage = () => {
-    title.innerHTML = 'Невозможно выполнить перевод';
+    showModalWindow(SERVICE_ERROR);
 };
 
 export const showResult = (text) => {
     output.value = text;
+};
+
+export const clearOutput = () => {
+    output.value = '';
+};
+
+export const showModalWindow = (text) => {
+    let modalWindow = document.createElement('div');
+    modalWindow.className = MODAL_WINDOW;
+    body.appendChild(modalWindow);
+
+    let closeButton = document.createElement('button');
+    closeButton.className = CLOSE_BUTTON;
+    closeButton.innerHTML = CROSS_CODE;
+    closeButton.addEventListener('click', closeModeWindow);
+    modalWindow.appendChild(closeButton);
+
+    let textField = document.createElement('div');
+    textField.className = TEXTFIELD;
+    textField.innerHTML = text;
+    modalWindow.appendChild(textField);
+
+    background.className = BACKGROUND_VISIBLE;
 };
--- src/js/logic.js
+++ src/js/logic.js
@@ -1,16 +1,20 @@
-import { showErrorMessage, showResult, showWarningMessage } from "./display";
+import { showErrorMessage, showResult, showWarningMessage, showModalWindow } from "./display";
 
 const textarea = document.querySelector('.input-form__textarea');
 const button = document.querySelector('.input-form__button');
+const languagesList = document.querySelector('.input-form__language');
 
 const URL = 'https://translate.yandex.net/api/v1.5/tr.json/';
 const DETECT_INSTRUCTION = 'detect';
 const TRANSLATE_INSTRUCTION = 'translate';
+const GETLANGS_INSTRUCTION = 'getLangs';
 const KEY = '?key=trnsl.1.1.20180220T094426Z.e1f4e967cf0ac2c7.8d56c206871ffd36c05216892008a97b3a312f5a';
 const TEXT_PREFIX = '&text=';
 const LANG_PREFIX = '&lang=';
 
-const activateButton = () => {
+let targetLanguage;
+
+const refreshButton = () => {
     if (textarea.value.length > 1) {
         button.removeAttribute('disabled');
     } else {
@@ -18,22 +22,55 @@ const activateButton = () => {
     }
 };
 
-async function translate() {
-    let response = await fetch(`${ URL }${ DETECT_INSTRUCTION }${ KEY }${ TEXT_PREFIX }${ encodeURI(textarea.value) }`)
+const buildLanguagesList = ({ langs }) => {
+    for (let language in langs) {
+        let option = document.createElement('option');
+        option.value = language;
+        option.innerHTML = langs[language];
+        languagesList.appendChild(option);
+    }
+
+    changeLanguage();
+};
+
+const changeLanguage = () => {
+    targetLanguage = `-${ languagesList.value }`;
+};
+
+async function getLanguages() {
+    let response = await fetch(`${ URL }${ GETLANGS_INSTRUCTION }${ KEY }&ui=ru`)
         .catch(showErrorMessage);
     let parsedResponse = await response.json();
 
-    if (parsedResponse.lang === '') {
-        return showWarningMessage();
-    }
-
-    response = await fetch(`${ URL }${ TRANSLATE_INSTRUCTION }${ KEY }${ TEXT_PREFIX }${ encodeURI(textarea.value) }${ LANG_PREFIX }${ parsedResponse.lang }-ru`)
-        .catch(showErrorMessage);
-    parsedResponse = await response.json();
-
-    showResult(parsedResponse.text);
-    textarea.value = '';
+    buildLanguagesList(parsedResponse);
 }
 
-textarea.addEventListener('input', activateButton);
+async function translate() {
+    try {
+        let response = await fetch(`${ URL }${ DETECT_INSTRUCTION }${ KEY }
+            ${ TEXT_PREFIX }${ encodeURI(textarea.value) }`);
+        let parsedResponse = await response.json();
+
+        if (parsedResponse.lang === '') {
+            return showWarningMessage();
+        }
+
+        response = await fetch(`${ URL }${ TRANSLATE_INSTRUCTION }${ KEY }
+            ${ TEXT_PREFIX }${ encodeURI(textarea.value) }${ LANG_PREFIX }${ parsedResponse.lang }${ targetLanguage }`);
+            parsedResponse = await response.json();
+
+        textarea.value = '';
+
+        showResult(parsedResponse.text);
+        showModalWindow(parsedResponse.text);
+        refreshButton();
+    } catch(error) {
+        showErrorMessage();
+    }
+}
+
+textarea.addEventListener('input', refreshButton);
 button.addEventListener('click', translate);
+languagesList.addEventListener('change', changeLanguage);
+
+getLanguages();
